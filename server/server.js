7c240665710e277232c99927b39b07c4310df4ee
griffin1995 <jtgriffin95@gmail.com>
2024-01-25 20:52:48 +0000

    revised, old dependancies removed

--- server/server.js
+++ server/server.js
@@ -1,17 +1,9 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+const express = require('express')
+
+// express app
+const app = express()
+
+// listen for requests
+app.listen(4000, () => {
+    console.log('Listening on port 4000')
+})
