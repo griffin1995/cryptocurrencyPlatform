df30cd26b5427feab05530b782bc80932a4b2351
griffin1995 <jtgriffin95@gmail.com>
2024-01-25 21:13:50 +0000

    express app setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
-const express = require('express')
+require("dotenv").config();
+
+const express = require("express");
 
 // express app
-const app = express()
+const app = express();
+//middleware - request/response logging
+app.use((request, response, next) => {
+  console.log(request.path, request.method);
+  next();
+});
+
+// routes
+app.get("/", (request, response) => {
+  response.json({
+    message: "Welcome to cryptoApp",
+  });
+});
 
 // listen for requests
-app.listen(4000, () => {
-    console.log('Listening on port 4000')
-})
+app.listen(process.env.PORT, () => {
+  console.log("Success. Listening on port 4000");
+});
+
+process.env;
