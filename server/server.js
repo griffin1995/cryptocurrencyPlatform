c3c984325ece19d9765d913c2f7ca54fae3e4144
griffin1995 <jtgriffin95@gmail.com>
2024-03-11 13:10:32 +0000

    dependancies

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const dataRoutes = require("./routes/data");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
@@ -11,16 +12,26 @@ const app = express();
 app.use(express.json());
 ////request/response logging
 app.use((request, response, next) => {
-    console.log(request.path, request.method);
-    next();
+  console.log(request.path, request.method);
+  next();
 });
 
 // routes
 app.use("/api/data", dataRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log("Success. Listening on port 4000");
-});
+// attempt to connec to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  //if success then:
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Success. Connected to the database.\nSuccess. Listening on port 4000.");
+    });
+  })
+  //else
+  .catch((error) => {
+    console.log(error);
+  });
 
 process.env;
