6a29f293b0e6a5c5e35a2cfbbd7b74c6751ae2f0
griffin1995 <jtgriffin95@gmail.com>
2024-03-13 11:50:44 +0000

    fetch data from api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [coins, setCoins] = useState(null);
+  useEffect(() => {
+    //we dont want useEffect to be async so we create function inside that does it
+    const fetchCoins = async () => {
+      const response = await fetch("http://localhost:4000/api/coins");
+      const json = await response.json();
+
+      if (response.ok) {
+        setCoins(json);
+      }
+    };
+    fetchCoins();
+  }, []);
+
+  
   return (
     <div className="home">
       <h2>Home</h2>
