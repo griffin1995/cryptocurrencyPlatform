aefc57e9a9f321de5a3b87553fc4c0ccdd5ffb1d
griffin1995 <jtgriffin95@gmail.com>
2024-03-13 10:05:06 +0000

    added relevant comments

--- server/controllers/coinController.js
+++ server/controllers/coinController.js
@@ -1,6 +1,8 @@
 // Import the Coin model to enable interactions with the 'coins' collection in the database.
 const Coin = require("../models/coinModel");
+// Import mongoose for MongoDB object modeling and to utilize its utilities, such as validating object IDs.
 const mongoose = require("mongoose");
+
 // Function to create a new coin entry in the database.
 const createCoin = async (request, response) => {
   // Extract 'name' and 'value' from the request body.
@@ -17,35 +19,33 @@ const createCoin = async (request, response) => {
   }
 };
 
-// Placeholder for a function to retrieve all coins from the database.
+// Function to retrieve all coins from the database.
 const getAllCoins = async (request, response) => {
+  // Fetch all coins from the database and sort them by creation date in descending order.
   const coins = await Coin.find({}).sort({ createdAt: -1 });
-  response.status(200).json(coins);
+  response.status(200).json(coins); // Respond with the fetched coins.
 };
 
-// Placeholder for a function to retrieve a single coin by its ID.
+// Function to retrieve a single coin by its ID.
 const getCoin = async (request, response) => {
-  const { id } = request.params;
+  const { id } = request.params; // Extract the coin's ID from the request parameters.
 
+  // Check if the provided ID is valid for a MongoDB ObjectID.
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return response.status(404).json({ error: "Can't find the coin" });
+    // If the ID is not valid, respond with a 404 status and an error message.
+    return response.status(404).json({ error: "Invalid id check for coin" });
   }
-  const coin = await Coin.findById(id);
+  const coin = await Coin.findById(id); // Find the coin by its ID in the database.
 
   if (!coin) {
+    // If no coin is found with the provided ID, respond with a 404 status and an error message.
     return response.status(404).json({ error: "Can't find the coin" });
   }
 
-  response.status(200).json(coin);
+  response.status(200).json(coin); // If a coin is found, respond with it.
 };
 
-// Placeholder for a function to delete a coin by its ID.
-// const deleteCoin = async (request, response) => {};
-
-// Placeholder for a function to update an existing coin's details by its ID.
-// const updateCoin = async (request, response) => {};
-
-// By exporting `createCoin`, we make it available for import using `require` in other parts of our application.
+// Exports the functions for use in other parts of the application, making them accessible for routing.
 module.exports = {
   createCoin,
   getAllCoins,
