d59469b55092849aac9c17610d877617e0e63638
griffin1995 <jtgriffin95@gmail.com>
2024-03-13 20:42:40 +0000

    added comments

--- client/src/components/CoinDetails.js
+++ client/src/components/CoinDetails.js
@@ -1,17 +1,22 @@
+// Define the CoinDetails component that receives a 'coin' object as a prop.
 const CoinDetails = ({ coin }) => {
   return (
+    // Container div for the coin details. Each coin's details will be displayed in this structured format.
     <div className="coin-details">
-      <h4>{coin.name}</h4>
+      <h4>{coin.name}</h4> {/* Display the coin's name. */}
       <p>
-        <strong>Value: </strong>
-        {coin.value}
+        <strong>Value: </strong> {/* Label for the coin's value. */}
+        {coin.value} {/* Display the coin's value. */}
       </p>
       <p>
-        <strong>Updated at: </strong>
-        {coin.updatedAt}
+        <strong>Updated at: </strong>{" "}
+        {/* Label for the coin's last updated timestamp. */}
+        {coin.updatedAt}{" "}
+        {/* Display the timestamp of when the coin was last updated. */}
       </p>
     </div>
   );
 };
 
+// Export the CoinDetails component for use in other parts of the application, such as in the Home component.
 export default CoinDetails;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 // Import useEffect for side effects and useState for state management from React.
 import { useEffect, useState } from "react";
 
+// Import the CoinDetails component to display individual coin details.
 import CoinDetails from "../components/CoinDetails";
 
 const Home = () => {
@@ -23,10 +24,10 @@ const Home = () => {
   return (
     <div className="home">
       <div className="coins">
-        {" "}
         {/* Conditionally render coins data if available. */}
-        {coins &&
-          coins.map((coin) => <CoinDetails key={coin._id} coin={coin} />)}
+        {coins && coins.map((coin) => (
+          <CoinDetails key={coin._id} coin={coin} />
+        ))}
       </div>
     </div>
   );
