40bfab0abac5a56b9a7c187004002af61b83d8af
Daniil Hristoforov Zhelyazkov <d.zhelyazkov@se22.qmul.ac.uk>
2024-03-18 19:19:02 +0000

    Logo Added

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -4,6 +4,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image"
+import Logo from "../media/CryptiQ_White.png"
 /**
  * Navbar component for the application.
  * Provides a top-level navigation element, allowing users to navigate through the app without full page reloads.
@@ -13,7 +15,9 @@ export default function NavigationBar() {
     <Row>
       <Navbar expand="lg" className="bg-primary">
         <Container>
-          <Navbar.Brand as={Link} to="/" className="text-white">LOGO</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/" className="text-white">
+            <Image fluid src={Logo} />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="justify-content-end flex-grow-1">
