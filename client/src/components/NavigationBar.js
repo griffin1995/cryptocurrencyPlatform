3c89856ea265784559d5255093c795ab096f7fdc
griffin1995 <jtgriffin95@gmail.com>
2024-03-19 09:57:33 +0000

    added empty user context file

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -4,8 +4,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Image from "react-bootstrap/Image"
-import Logo from "../media/CryptiQ_White.png"
+import Image from "react-bootstrap/Image";
+import Logo from "../media/CryptiQ_White.png";
 /**
  * Navbar component for the application.
  * Provides a top-level navigation element, allowing users to navigate through the app without full page reloads.
@@ -21,12 +21,24 @@ export default function NavigationBar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="justify-content-end flex-grow-1">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/Markets">Markets</Nav.Link>
-              <Nav.Link as={Link} to="/Blog">Blog</Nav.Link>
-              <Nav.Link as={Link} to="/SignUp">Sign Up</Nav.Link>
-              <Nav.Link as={Link} to="/LogIn">Log In</Nav.Link>
-              <Nav.Link as={Link} to="/Support">Support</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Markets">
+                Markets
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Blog">
+                Blog
+              </Nav.Link>
+              <Nav.Link as={Link} to="/SignUp">
+                Sign Up
+              </Nav.Link>
+              <Nav.Link as={Link} to="/LogIn">
+                Log In
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Support">
+                Support
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
