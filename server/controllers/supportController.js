028e6a2c2bcca42225f968e5e12d403497ad1ee3
griffin1995 <jtgriffin95@gmail.com>
2024-03-24 17:45:36 +0000

    created supportTicket controller, updated variables for supportTicketSchema

---
+++ server/controllers/supportController.js
@@ -0,0 +1,190 @@
+// Import required modules and models
+const SupportTicket = require("../models/supportTicketModel");
+const mongoose = require("mongoose");
+
+/**
+ * Creates a new support ticket.
+ *
+ * Extracts ticket information from the request body and uses the SupportTicket model
+ * to create a new ticket entry in the database. Returns the created ticket object
+ * or an error message upon failure.
+ *
+ * @param {Object} request - The request object containing the ticket information.
+ * @param {Object} response - The response object for sending back the created ticket or an error message.
+ */
+const createSupportTicket = async (request, response) => {
+  const {
+    userId,
+    status,
+    assignedSupportStaffId,
+    subject,
+    body,
+    priority,
+    category,
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    contactTime,
+  } = request.body;
+  try {
+    const ticket = await SupportTicket.create({
+      userId,
+      status,
+      assignedSupportStaffId,
+      subject,
+      body,
+      priority,
+      category,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      contactTime,
+    });
+    response.status(200).json(ticket);
+  } catch (error) {
+    response.status(400).json({
+      message:
+        "Failed to create support ticket. Please check your input and try again.",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * Retrieves all support tickets.
+ *
+ * Fetches all tickets from the database, sorts them by creation time in ascending order,
+ * and returns them. Returns an error message upon failure.
+ *
+ * @param {Object} request - The request object.
+ * @param {Object} response - The response object for sending back the tickets or an error message.
+ */
+const getAllTickets = async (request, response) => {
+  try {
+    const tickets = await SupportTicket.find({}).sort({ createdAt: 1 });
+    response.status(200).json(tickets);
+  } catch (error) {
+    response.status(500).json({
+      message: "Failed to retrieve tickets. Please try again later.",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * Retrieves a single support ticket by ID.
+ *
+ * Validates the ticket ID format and fetches the ticket from the database if valid.
+ * Returns the ticket object or an error message if the ticket is not found or in case of an error.
+ *
+ * @param {Object} request - The request object containing the ticket ID in params.
+ * @param {Object} response - The response object for sending back the ticket or an error message.
+ */
+const getTicket = async (request, response) => {
+  const { id } = request.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(404).json({
+      message: "Invalid ticket ID format. Please check the ID and try again.",
+    });
+  }
+
+  try {
+    const ticket = await SupportTicket.findById(id);
+    if (!ticket) {
+      return response.status(404).json({
+        message: "Ticket not found. Please check the ID and try again.",
+      });
+    }
+    response.status(200).json(ticket);
+  } catch (error) {
+    response.status(500).json({
+      message: "Error retrieving the ticket. Please try again later.",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * Deletes a support ticket by ID.
+ *
+ * Validates the ticket ID format and attempts to delete the corresponding ticket from the database.
+ * Returns a success message upon deletion or an error message if the ticket is not found or in case of an error.
+ *
+ * @param {Object} request - The request object containing the ticket ID in params.
+ * @param {Object} response - The response object for sending back the deletion status or an error message.
+ */
+const deleteTicket = async (request, response) => {
+  const { id } = request.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(404).json({
+      message:
+        "Invalid ticket ID format for deletion. Please check the ID and try again.",
+    });
+  }
+
+  try {
+    const ticket = await SupportTicket.findOneAndDelete({ _id: id });
+    if (!ticket) {
+      return response.status(404).json({
+        message:
+          "Ticket for deletion not found. Please check the ID and try again.",
+      });
+    }
+    response.status(200).json({ message: "Ticket deleted successfully." });
+  } catch (error) {
+    response.status(500).json({
+      message: "Error deleting the ticket. Please try again later.",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * Updates a support ticket by ID.
+ *
+ * Validates the ticket ID format and attempts to update the corresponding ticket with the request body data.
+ * Returns the updated ticket object upon success or an error message if the ticket is not found or in case of an error.
+ *
+ * @param {Object} request - The request object containing the updated ticket information and ticket ID in params.
+ * @param {Object} response - The response object for sending back the updated ticket or an error message.
+ */
+const updateTicket = async (request, response) => {
+  const { id } = request.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(404).json({
+      message:
+        "Invalid ticket ID format for update. Please check the ID and try again.",
+    });
+  }
+
+  try {
+    const ticket = await SupportTicket.findByIdAndUpdate(
+      { _id: id },
+      { ...request.body },
+      { new: true }
+    );
+    if (!ticket) {
+      return response.status(404).json({
+        message:
+          "Ticket for update not found. Please check the ID and try again.",
+      });
+    }
+    response.status(200).json(ticket);
+  } catch (error) {
+    response.status(500).json({
+      message: "Error updating the ticket. Please try again later.",
+      error: error.message,
+    });
+  }
+};
+
+// Export the controller functions to be used in router definitions
+module.exports = {
+  createSupportTicket,
+  getAllTickets,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+};
--- server/models/supportTicketModel.js
+++ server/models/supportTicketModel.js
@@ -1,15 +1,47 @@
 const mongoose = require("mongoose");
 
+// Define a Schema constructor from mongoose
 const Schema = mongoose.Schema;
 
+// Define the schema for a support ticket.
+// This schema includes fields for user identification, ticket status, assignment,
+// subject, body, priority, category, user contact information, and timestamps for creation and updates.
 const supportTicketSchema = new Schema(
   {
-    userID: { type: String, required: True }, //better name?
-    status: { type: Boolean, required: True }, //unanswered, replied, resolved?
-    supportID: { type: String, required: false }, //better name? i mean the staff thats helping
-    body: { type: String, required: True },
-    priority: { type: String, required: False },
-    department: { type: String, required: False },
+    // Unique identifier for the user who submitted the ticket.
+    userId: { type: String, required: true },
+    // Current status of the ticket, restricted to specific values with a default state.
+    status: {
+      type: String,
+      required: true,
+      enum: ["Unanswered", "Replied", "Resolved"],
+      default: "Unanswered",
+    },
+    // Identifier for the support staff assigned to the ticket, if any.
+    assignedSupportStaffId: { type: String, required: false },
+    // Brief title or subject of the support issue.
+    subject: { type: String, required: true },
+    // Detailed description of the issue or query.
+    body: { type: String, required: true },
+    // Priority level of the ticket, can be used to sort or triage tickets.
+    priority: { type: String, required: false },
+    // Category of the issue, helpful for routing or analysis.
+    category: { type: String, required: false },
+    // First name of the ticket submitter.
+    firstName: { type: String, required: true },
+    // Last name of the ticket submitter.
+    lastName: { type: String, required: true },
+    // Email address of the ticket submitter for communication.
+    email: { type: String, required: true },
+    // Optional phone number for alternative contact.
+    phoneNumber: { type: String, required: false },
+    // Preferred time for contact, if specified.
+    contactTime: { type: String, required: false },
   },
-  { timestamps: true }
+  {
+    timestamps: true, // Automatically add createdAt and updatedAt timestamps.
+  }
 );
+
+// Export the SupportTicket model, enabling CRUD operations on documents within the 'users' collection using the defined schema.
+module.exports = mongoose.model("SupportTicket", supportTicketSchema);
