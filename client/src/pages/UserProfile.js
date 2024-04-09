86ae16712d9dc5437a70c6c94513c060149da8da
ec22815 <ec22815@qmul.ac.uk>
2024-04-09 14:32:51 +0100

    UserProfile fetching data from api

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -3,46 +3,48 @@
 import React, { useState, useEffect } from 'react';
 import './UserProfile.scss'; 
 import UserDetails from '../components/UserDetails';
-import { useAdminContext } from '../hooks/useAdminContext';
 import { useAuthenticationContext } from '../hooks/useAuthenticationContext';
 
 const UserProfile = () => {
-
-    const [user, setUser] = useState(null);
+    const { user, dispatch} = useAuthenticationContext();
+    const [userData, setUserData] = useState(null);
 
     useEffect(() => {
-        // Simulate fetching user data (replace with actual fetch logic)
         const fetchUserData = async () => {
-        // Example user data (replace with actual user data retrieval)
-        const userData = {
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'johndoe@example.com',
-            phoneNumber: '1234567890',
-            updatedAt: '2024-04-10T12:30:00Z', // Example timestamp (should be ISO string)
-            _id: '123456789', // Example user ID
-            token: 'your-auth-token-here', // Example user token
-        };
-        setUser(userData);
-        };
-
+            try {
+                if (user) {
+                    const response = await fetch('/api/adminUserRoutes/profile', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                });
+      
+                if (response.ok) {
+                  const userData = await response.json(); // Parse response body to JSON
+                  setUserData(userData); // Set fetched user data to local state
+                } else {
+                  console.error('Failed to fetch user data');
+                }
+              }
+            } catch (error) {
+              console.error('Error fetching user data:', error);
+            }
+          };
+    
         fetchUserData();
     }, []);
-
+    
     return (
         <div className="user-profile">
-          <div className="user-details">
-            {user ? (
-              <>
-                <h4>{user.firstName} {user.lastName}</h4>
-                <p><strong>Email:</strong> {user.email}</p>
-                <p><strong>Phone Number:</strong> {user.phoneNumber}</p>
-              </>
+            {/* Render UserDetails component with user data if available */}
+            {userData ? (
+                <UserDetails user={userData} /> // Pass user data as prop to UserDetails component
             ) : (
-              <p>Loading user information...</p>
+            <p>Loading user information...</p>
             )}
-          </div>
-        </div>
+    </div>
     );
 };
 
