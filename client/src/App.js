6cf4c518c20a87adeec0514caeccb1f66b2850cf
Daniil Hristoforov Zhelyazkov <d.zhelyazkov@se22.qmul.ac.uk>
2024-04-10 14:22:42 +0100

    Log In Route fix

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import AdminControls from "./pages/AdminControls";
 import Blog from "./pages/Blog";
 import SignUp from "./pages/SignUp";
 // import LogIn from "./pages/LogIn";
-import LogIn from './pages/Login';
+import LogIn from './pages/LogIn';
 import Support from "./pages/Support";
 import Markets from "./pages/Markets";
 import Wallets from "./pages/Wallets";
