d937181a14fde79f57d95f0029f62f28dd42512e
griffin1995 <jtgriffin95@gmail.com>
2024-04-10 09:26:35 +0100

    wallet / trannsaction models

--- server/models/walletModel.js
+++ server/models/walletModel.js
@@ -3,26 +3,30 @@ const Schema = mongoose.Schema;
 
 // Schema definition for the Wallet model. This schema links to a User and manages their cryptocurrency holdings.
 const walletSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the User document to whom this wallet belongs.
-  holdings: [
+  name: { type: String, required: true },
+  assets: [
     {
-      coinId: { type: Schema.Types.ObjectId, ref: "Coin", required: true }, // Reference to the Coin document.
-      amountHeld: { type: Number, required: true }, // Amount of the coin currently held in the wallet.
+      coinId: { type: String, required: true },
+      amount: { type: Number, required: true },
     },
   ],
+  depositMoney: { type: Number, default: 0 },
 });
 
 // Method to add a new coin holding or update an existing one in the wallet.
 walletSchema.methods.modifyHolding = async function (coinId, amount) {
-  const index = this.holdings.findIndex((holding) =>
-    holding.coinId.equals(coinId)
-  );
+  const index = this.coins.findIndex((coin) => coin.coinId.equals(coinId));
+
   if (index >= 0) {
-    this.holdings[index].amountHeld += amount; // Update the amount held if the coin is already in the wallet.
+    this.coins[index].amountHeld += amount;
   } else {
-    this.holdings.push({ coinId, amountHeld: amount }); // Add a new holding if the coin is not already in the wallet.
+    this.coins.push({ coinId, amountHeld: amount });
   }
-  await this.save();
+  try {
+    await this.save();
+  } catch (error) {
+    throw new Error("Unable to modify");
+  }
 };
 
 const Wallet = mongoose.model("Wallet", walletSchema);
